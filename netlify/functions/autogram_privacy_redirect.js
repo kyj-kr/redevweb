22c6f92df8e73d4d5150deca844a1e353dd78f59
kyj-kr <82550751+kyj-kr@users.noreply.github.com>
2025-06-24 16:21:08 +0900

    done :)

---
+++ netlify/functions/autogram_privacy_redirect.js
@@ -0,0 +1,20 @@
+exports.handler = async (event, context) => {
+  const acceptLanguage = event.headers['accept-language'] || '';
+  
+  // 단순 언어 체크 (ko 포함 여부)
+  if (acceptLanguage.toLowerCase().includes('ko')) {
+    return {
+      statusCode: 302,
+      headers: {
+        Location: '/autogram_privacy_ko.html'
+      }
+    };
+  } else {
+    return {
+      statusCode: 302,
+      headers: {
+        Location: '/autogram_privacy.html'
+      }
+    };
+  }
+};
